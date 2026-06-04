a60f93644f08cb52ccc776fa48725e610bdf7042
liuandrew <14andrewliu@gmail.com>
2026-06-04 06:22:56 -0700

    update password

--- src/password.ts
+++ src/password.ts
@@ -2,4 +2,4 @@
 // To change the password, generate a new hash:
 //   node -e "const crypto = require('crypto'); console.log(crypto.createHash('sha256').update('your-password').digest('hex'));"
 export const PASSWORD_HASH =
-  "e08d706b3e4ce964b632746cf568913cb93f1ed36476fbb0494b80ed17c5975c";
+  "403779ed2792392f708f9e72512989c6f3f72b344d4f3d6e70f99bd33a004704";
